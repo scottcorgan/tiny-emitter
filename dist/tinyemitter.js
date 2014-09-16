81532e3600a6284a9cb87ef2f625e89df8f9282a
Scott Corgan <scottcorgan@gmail.com>
2014-09-16 12:10:16 -0700

    Bump to 1.0.0

--- dist/tinyemitter.js
+++ dist/tinyemitter.js
@@ -1,11 +1,11 @@
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.TinyEmitter=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function E () {
-  // Keep this empty so it's easier to inherit from
+	// Keep this empty so it's easier to inherit from
   // (via https://github.com/lipsmack from https://github.com/scottcorgan/tiny-emitter/issues/3)
 }
 
 E.prototype = {
-  on: function (name, callback, ctx) {
+	on: function (name, callback, ctx) {
     var e = this.e || (this.e = {});
     
     (e[name] || (e[name] = [])).push({
@@ -28,7 +28,7 @@ E.prototype = {
 
   emit: function (name) {
     var data = [].slice.call(arguments, 1);
-    var evtArr = (this.e[name] || []).slice();
+    var evtArr = ((this.e || (this.e = {}))[name] || []).slice();
     var i = 0;
     var len = evtArr.length;
     
@@ -53,6 +53,7 @@ E.prototype = {
     // Remove event from queue to prevent memory leak
     // Suggested by https://github.com/lazd
     // Ref: https://github.com/scottcorgan/tiny-emitter/commit/c6ebfaa9bc973b33d110a84a307742b7cf94c953#commitcomment-5024910
+
     (liveEvents.length) 
       ? e[name] = liveEvents
       : delete e[name];
@@ -62,6 +63,7 @@ E.prototype = {
 };
 
 module.exports = E;
+
 },{}]},{},[1])
 (1)
 });
