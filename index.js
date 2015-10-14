682e6e7b9f573e8509a56fff3761463d5d06a9a0
Scott Corgan <scottcorgan@gmail.com>
2015-10-14 12:14:51 -0700

    bug: deregister event set with once() method. Fixes #10

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ function E () {
 E.prototype = {
 	on: function (name, callback, ctx) {
     var e = this.e || (this.e = {});
-    
+
     (e[name] || (e[name] = [])).push({
       fn: callback,
       ctx: ctx
     });
-    
+
     return this;
   },
 
   once: function (name, callback, ctx) {
     var self = this;
-    var fn = function () {
-      self.off(name, fn);
+    function listener () {
+      self.off(name, listener);
       callback.apply(ctx, arguments);
     };
-    
-    return this.on(name, fn, ctx);
+
+    listener._ = callback
+    return this.on(name, listener, ctx);
   },
 
   emit: function (name) {
@@ -30,11 +31,11 @@ E.prototype = {
     var evtArr = ((this.e || (this.e = {}))[name] || []).slice();
     var i = 0;
     var len = evtArr.length;
-    
+
     for (i; i < len; i++) {
       evtArr[i].fn.apply(evtArr[i].ctx, data);
     }
-    
+
     return this;
   },
 
@@ -42,21 +43,22 @@ E.prototype = {
     var e = this.e || (this.e = {});
     var evts = e[name];
     var liveEvents = [];
-    
+
     if (evts && callback) {
       for (var i = 0, len = evts.length; i < len; i++) {
-        if (evts[i].fn !== callback) liveEvents.push(evts[i]);
+        if (evts[i].fn !== callback && evts[i].fn._ !== callback)
+          liveEvents.push(evts[i]);
       }
     }
-    
+
     // Remove event from queue to prevent memory leak
     // Suggested by https://github.com/lazd
     // Ref: https://github.com/scottcorgan/tiny-emitter/commit/c6ebfaa9bc973b33d110a84a307742b7cf94c953#commitcomment-5024910
 
-    (liveEvents.length) 
+    (liveEvents.length)
       ? e[name] = liveEvents
       : delete e[name];
-    
+
     return this;
   }
 };
--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var test = require('tape');
 test('subscribes to an event', function (t) {
   var emitter = new Emitter();
   emitter.on('test', function () {});
-  
+
   t.equal(emitter.e.test.length, 1, 'subscribed to event');
   t.end();
 });
@@ -14,23 +14,23 @@ test('subscribes to an event with context', function (t) {
   var context = {
     contextValue: true
   };
-  
+
   emitter.on('test', function () {
     t.ok(this.contextValue, 'is in context');
     t.end();
   }, context);
-  
+
   emitter.emit('test');
 });
 
 test('subscibes only once to an event', function (t) {
   var emitter = new Emitter();
-  
+
   emitter.once('test', function () {
     t.notOk(emitter.e.test, 'removed event from list');
     t.end();
   });
-  
+
   emitter.emit('test');
 });
 
@@ -39,36 +39,36 @@ test('keeps context when subscribed only once', function (t) {
   var context = {
     contextValue: true
   };
-  
+
   emitter.once('test', function () {
     t.ok(this.contextValue, 'is in context');
     t.notOk(emitter.e.test, 'not subscribed anymore');
     t.end();
   }, context);
-  
+
   emitter.emit('test');
 });
 
 test('emits an event', function (t) {
   var emitter = new Emitter();
-  
+
   emitter.on('test', function () {
     t.ok(true, 'triggered event');
     t.end();
   });
-  
+
   emitter.emit('test');
 });
 
 test('passes all arguments to event listener', function (t) {
   var emitter = new Emitter();
-  
+
   emitter.on('test', function (arg1, arg2) {
     t.equal(arg1, 'arg1', 'passed the first argument');
     t.equal(arg2, 'arg2', 'passed the second argument');
     t.end();
   });
-  
+
   emitter.emit('test', 'arg1', 'arg2');
 });
 
@@ -79,7 +79,7 @@ test('unsubscribes from all events with name', function (t) {
   });
   emitter.off('test');
   emitter.emit('test')
-  
+
   process.nextTick(function () {
     t.end();
   });
@@ -90,11 +90,11 @@ test('unsubscribes single event with name and callback', function (t) {
   var fn = function () {
     t.ok(false, 'should not get called');
   }
-  
+
   emitter.on('test', fn);
   emitter.off('test', fn);
   emitter.emit('test')
-  
+
   process.nextTick(function () {
     t.end();
   });
@@ -110,10 +110,10 @@ test('unsubscribes single event with name and callback when subscribed twice', f
 
   emitter.on('test', fn);
   emitter.on('test', fn);
-  
+
   emitter.off('test', fn);
   emitter.emit('test');
-  
+
   process.nextTick(function () {
     t.notOk(emitter.e['test'], 'removes all events');
     t.end();
@@ -144,16 +144,16 @@ test('unsubscribes single event with name and callback when subscribed twice out
 
 test('removes an event inside another event', function (t) {
   var emitter = new Emitter();
-  
+
   emitter.on('test', function () {
     t.equal(emitter.e.test.length, 1, 'event is still in list');
-    
+
     emitter.off('test');
-    
+
     t.notOk(emitter.e.test, 0, 'event is gone from list');
     t.end();
   });
-  
+
   emitter.emit('test');
 });
 
@@ -164,22 +164,22 @@ test('event is emitted even if unsubscribed in the event callback', function (t)
     calls += 1;
     emitter.off('test', fn);
   };
-  
+
   emitter.on('test', fn);
-  
+
   emitter.on('test', function () {
     calls += 1;
   });
-  
+
   emitter.on('test', function () {
     calls += 1;
   });
-  
+
   process.nextTick(function () {
     t.equal(calls, 3, 'all callbacks were called');
     t.end();
   });
-  
+
   emitter.emit('test');
 });
 
@@ -191,7 +191,7 @@ test('calling off before any events added does nothing', function (t) {
 
 test('emitting event that has not been subscribed to yet', function (t) {
   var emitter = new Emitter();
-  
+
   emitter.emit('some-event', 'some message');
   t.end();
 });
@@ -199,14 +199,12 @@ test('emitting event that has not been subscribed to yet', function (t) {
 test('unsubscribes single event with name and callback which was subscribed once', function (t) {
   var emitter = new Emitter();
   var fn = function () {
-    t.ok(false, 'should not get called');
+    t.fail('event not unsubscribed');
   }
 
   emitter.once('test', fn);
   emitter.off('test', fn);
-  emitter.emit('test')
+  emitter.emit('test');
 
-  process.nextTick(function () {
-    t.end();
-  });
+  t.end();
 });
