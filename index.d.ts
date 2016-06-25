58f6529b4a03b361b22e3d54240ef8b8e146599a
Endel Dreyer <endel.dreyer@gmail.com>
2016-06-25 20:11:34 +0200

    'new' method is unecessary

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 declare module "tiny-emitter" {
   class EventEmitter {
-    new(...args:any[]): EventEmitter;
     on   (event: string, callback: Function, ctx?: any): EventEmitter;
     once (event: string, callback: Function, ctx?: any): EventEmitter;
     emit (event: string, ...args: any[]): EventEmitter;
