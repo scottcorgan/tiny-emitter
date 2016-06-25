027eff86315ef5b72f3f9365bb092a2ffb91c7af
Endel Dreyer <endel.dreyer@gmail.com>
2016-06-25 20:21:27 +0200

    shouldn't be a module

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,7 @@
-declare module "tiny-emitter" {
-  class EventEmitter {
-    on   (event: string, callback: Function, ctx?: any): EventEmitter;
-    once (event: string, callback: Function, ctx?: any): EventEmitter;
-    emit (event: string, ...args: any[]): EventEmitter;
-    off  (event: string, callback?: Function): EventEmitter;
-  }
-  export = EventEmitter
+declare class EventEmitter {
+  on   (event: string, callback: Function, ctx?: any): EventEmitter;
+  once (event: string, callback: Function, ctx?: any): EventEmitter;
+  emit (event: string, ...args: any[]): EventEmitter;
+  off  (event: string, callback?: Function): EventEmitter;
 }
+export = EventEmitter
