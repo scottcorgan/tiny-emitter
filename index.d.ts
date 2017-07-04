112d3a6e1d7fa199441228742fbc2a889b770076
Artur Ciocanu <ciocanu@adobe.com>
2017-07-04 18:27:36 +0300

    Making sure we can import as ECMAScript 2015 module

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
-declare class EventEmitter {
+export declare class EventEmitter {
   on   (event: string, callback: Function, ctx?: any): EventEmitter;
   once (event: string, callback: Function, ctx?: any): EventEmitter;
   emit (event: string, ...args: any[]): EventEmitter;
   off  (event: string, callback?: Function): EventEmitter;
 }
-export = EventEmitter
