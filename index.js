60753e5d33ac3a464831c20a3dde55c3d482d134
Black Cat <me@felis.cat>
2017-09-23 18:45:03 +0300

    Stuff
    above

--- index.js
+++ index.js
@@ -1,66 +1,37 @@
-function E () {
-  // Keep this empty so it's easier to inherit from
-  // (via https://github.com/lipsmack from https://github.com/scottcorgan/tiny-emitter/issues/3)
-}
-
-E.prototype = {
-  on: function (name, callback, ctx) {
-    var e = this.e || (this.e = {});
-
-    (e[name] || (e[name] = [])).push({
-      fn: callback,
-      ctx: ctx
-    });
-
-    return this;
-  },
-
-  once: function (name, callback, ctx) {
-    var self = this;
-    function listener () {
-      self.off(name, listener);
-      callback.apply(ctx, arguments);
-    };
-
-    listener._ = callback
-    return this.on(name, listener, ctx);
-  },
-
-  emit: function (name) {
-    var data = [].slice.call(arguments, 1);
-    var evtArr = ((this.e || (this.e = {}))[name] || []).slice();
-    var i = 0;
-    var len = evtArr.length;
-
-    for (i; i < len; i++) {
-      evtArr[i].fn.apply(evtArr[i].ctx, data);
+class Emitter {
+  emit(event, ...args) {
+    if (!event) return this;
+    for (const fn of this._e(event)) {
+      fn.apply(fn.ctx, [...args]);
+      if (fn.off_event == true) this.off(event, fn);
     }
-
-    return this;
-  },
-
-  off: function (name, callback) {
-    var e = this.e || (this.e = {});
-    var evts = e[name];
-    var liveEvents = [];
-
-    if (evts && callback) {
-      for (var i = 0, len = evts.length; i < len; i++) {
-        if (evts[i].fn !== callback && evts[i].fn._ !== callback)
-          liveEvents.push(evts[i]);
-      }
-    }
-
-    // Remove event from queue to prevent memory leak
-    // Suggested by https://github.com/lazd
-    // Ref: https://github.com/scottcorgan/tiny-emitter/commit/c6ebfaa9bc973b33d110a84a307742b7cf94c953#commitcomment-5024910
-
-    (liveEvents.length)
-      ? e[name] = liveEvents
-      : delete e[name];
-
     return this;
   }
-};
-
-module.exports = E;
+  on(event, fn, ctx) {
+    if (!event) return this;
+    fn.ctx = ctx;
+    this._e(event).push(fn);
+    return this;
+  }
+  once(event, fn, ctx) {
+    if (!event) return this;
+    fn.ctx = ctx;
+    fn.off_event = true;
+    return this.on(event, fn);
+  }
+  off(event, fn) {
+    if (!event) return this;
+    if (!this[event]) return this;
+    const e = this._e(event);
+    if (!fn) {
+      delete this[event];
+      return this;
+    }
+    this[event] = e.filter((f) => f != fn);
+    return this;
+  }
+  _e(e) {
+    return this[e] || (this[e] = []);
+  }
+}
+module.exports = Emitter;
