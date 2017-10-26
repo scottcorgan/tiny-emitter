557c84fa973ac463221b0b24da36a1409db94dea
Neek Sandhu <neek.sandhu@outlook.com>
2017-10-26 14:23:25 -0700

    Update API + migrate to ES6+

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,7 @@
 declare class EventEmitter {
   on   (event: string, callback: Function, ctx?: any): EventEmitter;
-  once (event: string, callback: Function, ctx?: any): EventEmitter;
   emit (event: string, ...args: any[]): EventEmitter;
   off  (event: string, callback?: Function): EventEmitter;
 }
 
-export = EventEmitter
+export default EventEmitter
