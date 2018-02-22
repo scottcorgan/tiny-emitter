10efb9ac6aaff4d03260bbad9f5a14d90814118a
Madara Uchiha <me@madara.ninja>
2018-02-22 22:43:45 +0200

    Correct TinyEmitter's types - Fixes #29
    - Add .TinyEmitter to module.exports exposing E from that key.
    - Remove the faulty ability to `import * as TinyEmitter` in TypeScript
    - Add the ability to `import { TinyEmitter }` in TypeScript
    - Change all method return types to return `this` instead of
      `TinyEmitter`, this way, extending classes would have correct return
      types.

--- dist/tinyemitter.js
+++ dist/tinyemitter.js
@@ -1,5 +1,5 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.TinyEmitter = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-function E () {
+function E() {
   // Keep this empty so it's easier to inherit from
   // (via https://github.com/lipsmack from https://github.com/scottcorgan/tiny-emitter/issues/3)
 }
@@ -18,7 +18,7 @@ E.prototype = {
 
   once: function (name, callback, ctx) {
     var self = this;
-    function listener () {
+    function listener() {
       self.off(name, listener);
       callback.apply(ctx, arguments);
     };
@@ -65,6 +65,7 @@ E.prototype = {
 };
 
 module.exports = E;
+module.exports.TinyEmitter = E;
 
 },{}]},{},[1])(1)
 });
