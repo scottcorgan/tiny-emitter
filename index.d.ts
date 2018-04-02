8048fffc6a3ad5b5f551fcc02d9aa74f6051082e
Robert Laverty <roblav96@gmail.com>
2018-04-02 01:42:19 -0400

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,23 @@
-export declare class TinyEmitter {
-  on(event: string, callback: Function, ctx?: any): this;
-  once(event: string, callback: Function, ctx?: any): this;
-  emit(event: string, ...args: any[]): this;
-  off(event: string, callback?: Function): this;
+
+declare module 'tiny-emitter' {
+
+	namespace TinyEmitter {
+		interface Event<D> {
+			fn: Listener<D>
+			ctx: any
+		}
+		type Events<D> = { [event: string]: Event<D>[] };
+		type Listener<D> = (...args: D[]) => void;
+	}
+
+	class TinyEmitter<E = string, D = any> {
+		e: TinyEmitter.Events<D>
+		on(event: E, listener: TinyEmitter.Listener<D>, ctx?: any): this;
+		once(event: E, listener: TinyEmitter.Listener<D>, ctx?: any): this;
+		emit(event: E, ...args: D[]): this;
+		off(event: E, listener?: TinyEmitter.Listener<D>): this;
+	}
+
+	export = TinyEmitter;
+
 }
