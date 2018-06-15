fcdec560fd616726f73de0565278a13caa301137
Yao Ding <yaodingyd@gmail.com>
2018-06-15 10:58:15 -0400

    checking e is array instead of just defined
    This fix if we are trying to intentional overwrite object's native properties as event name, like 'toString'

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ E.prototype = {
   on: function (name, callback, ctx) {
     var e = this.e || (this.e = {});
 
-    (e[name] || (e[name] = [])).push({
+    (Array.isArray(e[name]) || (e[name] = [])).push({
       fn: callback,
       ctx: ctx
     });
